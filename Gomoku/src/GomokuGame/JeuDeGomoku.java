dd8601a70ea45dfbbff8ac9033a243a0000d89cd
AGALDEANO <alexandre.galdeano@etu.univ-lyon1.fr>
2014-04-09 09:49:16 +0200

    Method joueurSuivant in class JeuDeGomoku implemented

--- Gomoku/src/GomokuGame/JeuDeGomoku.java
+++ Gomoku/src/GomokuGame/JeuDeGomoku.java
@@ -15,19 +15,46 @@ public class JeuDeGomoku {
     private Joueur joueurs[];
     private Joueur joueurCourant;
     private PlateauGomoku plateau;
+    private int dernierIndex;
     private boolean premierCoup;
-    public JeuDeGomoku(){};
-    public void setJoueur(int ordre, Joueur joueur)
+    public JeuDeGomoku()
     {
-        
+        joueurs = new Joueur[2];
+        joueurs[0] = new JoueurAleatoire(1);
+        joueurs[1] = new JoueurAleatoire(2);
+        plateau = new PlateauGomoku();
+        premierCoup=true;
+        dernierIndex = Utilitaire.monRandom(0, 1);
+        joueurCourant = joueurs[dernierIndex];
     }
-    public void setPlateau(PlateauGomoku plateau)
+    public JeuDeGomoku(Joueur joueur1, Joueur joueur2)
     {
-        
+        joueurs = new Joueur[2];
+        joueurs[0] = joueur1;
+        joueurs[1] = joueur2;
+        plateau = new PlateauGomoku();
+        premierCoup=true;
+        dernierIndex = Utilitaire.monRandom(0, 1);
+        joueurCourant = joueurs[dernierIndex];
+    }
+    public void setJoueur(int _ordre, Joueur _joueur) throws java.lang.IndexOutOfBoundsException
+    {
+        if(_ordre<0||_ordre>=joueurs.length) throw new java.lang.IndexOutOfBoundsException();
+        else joueurs[_ordre]=_joueur;
+    }
+    public void setPlateau(PlateauGomoku _plateau)
+    {
+        plateau = _plateau;
     }
     private Joueur joueurSuivant()
     {
-        return new JoueurAleatoire(0);
+        int _idCourant = getDernierId();
+        if(_idCourant == 0) return joueurCourant;
+        else
+        {
+            dernierIndex=(dernierIndex+1)%joueurs.length;
+            return joueurs[dernierIndex];
+        }
     }
     public int getDernierId()
     {
